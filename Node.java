8159d4fc44263d34f20c85a05a62e1c7ecf7a4d7
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2023-09-17 15:58:05 -0300

    Add author names

--- Node.java
+++ Node.java
@@ -1,3 +1,10 @@
+/**
+ * //TODO: description
+ * @author André Matteucci - //TODO: TIA
+ * @author Felipe Ribeiro  - 32212720
+ * @author Enzo Koji       - //TODO: TIA
+*/
+
 public class Node {
     char data;
     Node left;
