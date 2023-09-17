af9662c47fa126ddfa4ec7a954adc2baac1dffca
hashaski <matteucciandre@gmail.com>
2023-09-17 16:17:44 -0300

    Meu TIA adicionado

--- Node.java
+++ Node.java
@@ -1,6 +1,6 @@
 /**
  * //TODO: description
- * @author André Matteucci - //TODO: TIA
+ * @author André Matteucci - 32273541
  * @author Felipe Ribeiro  - 32212720
  * @author Enzo Koji       - //TODO: TIA
 */
