52a54fe2c239c7719727c602a9e07fd9ea89dce5
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2023-09-17 15:51:40 -0300

    Add Node.java

---
+++ Node.java
@@ -0,0 +1,41 @@
+public class Node {
+    char data;
+    Node left;
+    Node right;
+
+    public Node(char data) {
+        this.data = data;
+        this.left = null;
+        this.right = null;
+    }
+
+    public Node(char data, Node left, Node right) {
+        this.data = data;
+        this.left = left;
+        this.right = right;
+    }
+
+    public void setData(char data) {
+        this.data = data;
+    }
+    
+    public void setLeft(Node left) {
+        this.left = left;
+    }
+
+    public void setRight(Node right) {
+        this.right = right;
+    }
+
+    public char getData() {
+        return this.data;
+    }
+
+    public Node getLeft() {
+        return this.left;
+    }
+
+    public Node getRight() {
+        return this.right;
+    }
+}
