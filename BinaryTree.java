95ae0673b83875ba269efd5b421f86d1e7996dd4
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2023-09-22 00:03:55 -0300

    Add level order traversal method

--- BinaryTree.java
+++ BinaryTree.java
@@ -5,6 +5,9 @@
  * @author Enzo Koji       - 32273754
 */
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class BinaryTree {
     private Node root;
 
@@ -78,6 +81,37 @@ public class BinaryTree {
 		traversal.append(postorderTraversal(node.getLeft()));
 		traversal.append(postorderTraversal(node.getRight()));
 		traversal.append(node.getData() + " ");
+		
+		return traversal.toString();
+	}
+
+	public String levelOrderTraversal() {
+		return levelOrderTraversal(root);
+	}
+	
+	private String levelOrderTraversal(Node node) {
+		if (node == null) {
+			return "";
+		}
+		
+		StringBuilder traversal = new StringBuilder();
+
+		Queue<Node> queue = new LinkedList<>();
+
+		queue.add(node);
+
+		while (!queue.isEmpty()) {
+			Node visited = queue.remove();
+			traversal.append(visited.getData() + " ");
+
+			if (visited.hasLeft()) {
+				queue.add(visited.getLeft());
+			}
+
+			if (visited.hasRight()) {
+				queue.add(visited.getRight());
+			}
+		}
 		
 		return traversal.toString();
 	}
