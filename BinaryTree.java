8a9117edfabc447013bfc640614a042470b8024e
Felipe Ribeiro <117246298+exh-Auster@users.noreply.github.com>
2023-09-21 23:59:59 -0300

    Add tree traversal methods

--- BinaryTree.java
+++ BinaryTree.java
@@ -27,4 +27,58 @@ public class BinaryTree {
     public void setRoot(Node root) {
         this.root = root;
     }
+
+    public String inorderTraversal() {
+		return inorderTraversal(root);
+	}
+
+	private String inorderTraversal(Node node) {
+		if (node == null) {
+			return "";
+		}
+
+		StringBuilder traversal = new StringBuilder();
+		
+		traversal.append(inorderTraversal(node.getLeft()));
+		traversal.append(node.getData() + " ");
+		traversal.append(inorderTraversal(node.getRight()));
+		
+		return traversal.toString();
+	}
+
+	public String preorderTraversal() {
+		return preorderTraversal(root);
+	}
+
+	private String preorderTraversal(Node node) {
+		if (node == null) {
+			return "";
+		}
+
+		StringBuilder traversal = new StringBuilder();
+		
+		traversal.append(node.getData() + " ");
+		traversal.append(preorderTraversal(node.getLeft()));
+		traversal.append(preorderTraversal(node.getRight()));
+		
+		return traversal.toString();
+	}
+
+	public String postorderTraversal() {
+		return postorderTraversal(root);
+	}
+
+	private String postorderTraversal(Node node) {
+		if (node == null) {
+			return "";
+		}
+
+		StringBuilder traversal = new StringBuilder();
+		
+		traversal.append(postorderTraversal(node.getLeft()));
+		traversal.append(postorderTraversal(node.getRight()));
+		traversal.append(node.getData() + " ");
+		
+		return traversal.toString();
+	}
 }
